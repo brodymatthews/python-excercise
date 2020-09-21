9a89494741c233671ae2d327932ed19ee4719f3e
brody <brodin.matthews@gmail.com>
2020-09-21 11:04:05 +0200

    First days work

---
+++ day1.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+class_name = "class 2"
+
+print("I was coded in " + class_name)
+
+class_student = "19"
+
+print ("There are " + class_student + " students in " + class_name)
